24fb46a0c8572359560678a13ec38c70f6cae12c
yxie <yangxie@umd.edu>
2018-02-09 14:53:27 -0500

    modify lec7 code

--- examples/07_convnet_mnist_starter.py
+++ examples/07_convnet_mnist_starter.py
@@ -7,7 +7,7 @@ Lecture 07
 """
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-import time 
+import time
 
 import tensorflow as tf
 
@@ -20,14 +20,30 @@ def conv_relu(inputs, filters, k_size, stride, padding, scope_name):
     #############################
     ########## TO DO ############
     #############################
-    return None
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        c_r = tf.layers.conv2d(
+            inputs = inputs,
+            filters = filters,
+            kernel_size = k_size,
+            strides = stride,
+            padding = padding,
+            activation = tf.nn.relu
+        )
+        return c_r
 
 def maxpool(inputs, ksize, stride, padding='VALID', scope_name='pool'):
     '''A method that does max pooling on inputs'''
     #############################
     ########## TO DO ############
     #############################
-    return None
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        mp = tf.layers.max_pooling2d(
+            inputs = inputs,
+            pool_size = ksize,
+            strides = stride,
+            padding = padding
+        )
+        return mp
 
 def fully_connected(inputs, out_dim, scope_name='fc'):
     '''
@@ -36,23 +52,30 @@ def fully_connected(inputs, out_dim, scope_name='fc'):
     #############################
     ########## TO DO ############
     #############################
-    return None
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        fc = tf.layers.dense(
+            inputs=inputs,
+            units=out_dim,
+            activation=tf.nn.relu
+        )
+        return fc
 
 class ConvNet(object):
     def __init__(self):
         self.lr = 0.001
         self.batch_size = 128
         self.keep_prob = tf.constant(0.75)
-        self.gstep = tf.Variable(0, dtype=tf.int32, 
+        self.gstep = tf.Variable(0, dtype=tf.int32,
                                 trainable=False, name='global_step')
         self.n_classes = 10
         self.skip_step = 20
         self.n_test = 10000
+        self.training = False
 
     def get_data(self):
         with tf.name_scope('data'):
             train_data, test_data = utils.get_mnist_dataset(self.batch_size)
-            iterator = tf.data.Iterator.from_structure(train_data.output_types, 
+            iterator = tf.data.Iterator.from_structure(train_data.output_types,
                                                    train_data.output_shapes)
             img, self.label = iterator.get_next()
             self.img = tf.reshape(img, shape=[-1, 28, 28, 1])
@@ -68,7 +91,51 @@ class ConvNet(object):
         #############################
         ########## TO DO ############
         #############################
-        self.logits = None
+        # conv_relu(inputs, filters, k_size, stride, padding, scope_name)
+        # maxpool(inputs, ksize, stride, padding='VALID', scope_name='pool')
+        # fully_connected(inputs, out_dim, scope_name='fc')
+
+        conv1 = conv_relu(
+            inputs = self.img,
+            filters = 32,
+            k_size = [5,5],
+            stride = 1,
+            padding = 'SAME',
+            scope_name = 'conv1'
+        )
+        maxpool1 = maxpool(
+            inputs = conv1,
+            ksize = [2,2],
+            stride = 2,
+            scope_name = 'maxpool1'
+        )
+        conv2 = conv_relu(
+            inputs = maxpool1,
+            filters = 64,
+            k_size = [5,5],
+            stride = 1,
+            padding = 'SAME',
+            scope_name = 'conv2'
+        )
+        maxpool2 = maxpool(
+            inputs = conv2,
+            ksize = [2,2],
+            stride = 2,
+            scope_name = 'maxpool2'
+        )
+        # reshape maxpool2 to a 1-d vector?
+        feature_dim = maxpool2.shape[1] * maxpool2.shape[2] * maxpool2.shape[3]
+        maxpool2 = tf.reshape(maxpool2, [-1, feature_dim])
+        fc1 = fully_connected(
+            inputs = maxpool2,
+            out_dim = 1024,
+            scope_name = 'fc1'
+        )
+        dropout = tf.layers.dropout(fc1,
+                                    self.keep_prob,
+                                    training=self.training,
+                                    name='dropout')
+        self.logits = tf.layers.dense(dropout, self.n_classes, name='logits')
 
     def loss(self):
         '''
@@ -81,8 +148,14 @@ class ConvNet(object):
         #############################
         ########## TO DO ############
         #############################
-        self.loss = None
-    
+        with tf.name_scope('loss'):
+            entropy = tf.nn.softmax_cross_entropy_with_logits(
+                labels = self.label,
+                logits = self.logits,
+                name = 'entropy'
+            )
+            self.loss = tf.reduce_mean(entropy, name = 'loss')
+
     def optimize(self):
         '''
         Define training op
@@ -92,7 +165,8 @@ class ConvNet(object):
         #############################
         ########## TO DO ############
         #############################
-        self.opt = None
+        self.opt = tf.train.AdamOptimizer(self.lr).minimize(self.loss,
+                                                global_step=self.gstep)
 
     def summary(self):
         '''
@@ -102,8 +176,12 @@ class ConvNet(object):
         #############################
         ########## TO DO ############
         #############################
-        self.summary_op = None
-        
+        with tf.name_scope('summaries'):
+            tf.summary.scalar('loss', self.loss)
+            tf.summary.scalar('accuracy', self.accuracy)
+            tf.summary.histogram('histogram loss', self.loss)
+            self.summary_op = tf.summary.merge_all()
+
     def eval(self):
         '''
         Count the number of right predictions in a batch
@@ -126,7 +204,8 @@ class ConvNet(object):
 
     def train_one_epoch(self, sess, saver, init, writer, epoch, step):
         start_time = time.time()
-        sess.run(init) 
+        sess.run(init)
+        self.training = True
         total_loss = 0
         n_batches = 0
         try:
@@ -148,6 +227,7 @@ class ConvNet(object):
     def eval_once(self, sess, init, writer, epoch, step):
         start_time = time.time()
         sess.run(init)
+        self.training = False
         total_correct_preds = 0
         try:
             while True:
@@ -174,7 +254,7 @@ class ConvNet(object):
             ckpt = tf.train.get_checkpoint_state(os.path.dirname('checkpoints/convnet_starter/checkpoint'))
             if ckpt and ckpt.model_checkpoint_path:
                 saver.restore(sess, ckpt.model_checkpoint_path)
-            
+
             step = self.gstep.eval()
 
             for epoch in range(n_epochs):
